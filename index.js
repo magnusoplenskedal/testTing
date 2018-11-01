94a28f338ea754f71377e07d1ddf88066821cf81
magnusko oplenskedal <magnusko88@gmail.com>
2018-11-01 17:29:15 +0100

    simple node app with test

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
+
+app.listen(process.env.PORT || 3000);
+
+module.exports = app;
