2db86462a4fd8cb10131077c533ec2d04ab4d92e
magnusko oplenskedal <magnusko88@gmail.com>
2018-11-01 20:31:29 +0100

    testing monitoring from server

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var app = express();
 
 app.get("/", (req, res) => {
     console.log("received requrest");
-    res.send("hello world med npm install");
+    res.send("Mega test");
 });
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("testTing is listeing on port 3000");
+    console.log("testTing is listening on port 3000");
 });
 
 module.exports = app;
